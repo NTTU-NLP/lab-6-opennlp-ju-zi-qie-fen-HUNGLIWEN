72a55881d7767ff10dc1b66125bfcf2a2cfc3d34
HUNGLIWEN <a0917506272@gmail.com>
2017-04-09 11:00:59 +0800

    10311171 lab6

---
+++ lab6/lab6/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using java.io;
+using opennlp.tools.tokenize;
+using System.Text.RegularExpressions;
+
+namespace lab6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] file = Directory.GetFiles(@"C:\10311171\lab-6-opennlp-ju-zi-qie-fen-HUNGLIWEN\lab6\Dataset", "*.html");
+            StreamWriter sw = new StreamWriter(@"ReadByTokens.txt");
+            foreach (string filename in file)
+                using (StreamReader sr = new StreamReader(filename))
+                {
+
+                    while (sr.Peek() != -1)
+                    {
+                        string line = sr.ReadLine();
+                        string[] tokens;
+                        InputStream modelIn = new FileInputStream(@"C:\10311171\lab-6-opennlp-ju-zi-qie-fen-HUNGLIWEN\en-token.bin");
+                        TokenizerModel model = new TokenizerModel(modelIn);
+                        TokenizerME enTokenizer = new TokenizerME(model);
+                        tokens = enTokenizer.tokenize(line);
+                        for (int i = 0; i < tokens.Length; i++)
+                        {
+                            sw.Write(tokens[i] + " ");
+
+                            if (tokens[i].Equals("."))
+                            {
+                                sw.Write("\n");
+                            }
+                        }
+                    }
+                }
+            sw.Close();
+        }
+    }
+}
